7b741730a0370b421c286b1d89a8bf8d65d8281e
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-10-16 22:02:36 +0300

    refactor(yandex_search.py): switch var name

--- yandex_search.py
+++ yandex_search.py
@@ -3,6 +3,7 @@
 
 import requests, bs4, webbrowser
 
+
 def search(response):
     request = requests.get("https://yandex.ru/search/?text=" + response)
     try:
@@ -14,10 +15,11 @@ def search(response):
     results = [{'text': tag.get_text(), 'link': tag["href"]} for tag in soup.select("a.link_cropped_no")]
     return results
 
+
 if __name__ == "__main__":
     print("Enter your response:", end='')
-    response = input()
-    results = search(response)
+    request = input()
+    results = search(request)
     print('\n'.join((str(num) + ". " + result['text'] for (num, result) in enumerate(results))))
 
     flag = True
