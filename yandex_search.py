116f27a876d12b7378ac673cc2446dcc30d9a10a
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-11-11 23:51:25 +0300

    feat(yandex_search.py): replace dict with namedtuple

--- yandex_search.py
+++ yandex_search.py
@@ -1,20 +1,23 @@
 #! python3
 # Yandex search
 import webbrowser
+from collections import namedtuple
 
 import requests
 import bs4
+
+SearchResult = namedtuple('SearchResult', 'text link')
 
 
 def search(subject):
     request = requests.get("https://yandex.ru/search/?text=" + subject)
     try:
         request.raise_for_status()
-    except:
+    except requests.exceptions.HTTPError:
         print("Sorry, some errors occurred.")
 
     soup = bs4.BeautifulSoup(request.text, "html.parser")
-    results = [{'text': tag.get_text(), 'link': tag["href"]}
+    results = [SearchResult(tag.get_text(), tag["href"])
                for tag in soup.select("a.link_cropped_no")]
     return results
 
@@ -33,7 +36,7 @@ def main():
         print("Enter your choice: ", end='')
         try:
             choice = results[int(input())]['link']
-        except:
+        except Exception:
             print("Sorry, some errors occurred. Check your input.")
         else:
             webbrowser.open(choice)
