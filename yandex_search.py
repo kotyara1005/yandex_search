1b1b5d1cada627998dfd298f631516e8632d546a
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2016-11-11 23:45:34 +0300

    refactor(yandex_search.py): PEP8

--- yandex_search.py
+++ yandex_search.py
@@ -1,30 +1,36 @@
 #! python3
 # Yandex search
+import webbrowser
 
-import requests, bs4, webbrowser
+import requests
+import bs4
 
 
-def search(response):
-    request = requests.get("https://yandex.ru/search/?text=" + response)
+def search(subject):
+    request = requests.get("https://yandex.ru/search/?text=" + subject)
     try:
         request.raise_for_status()
     except:
         print("Sorry, some errors occurred.")
 
-    soup = bs4.BeautifulSoup(request.text, "lxml")
-    results = [{'text': tag.get_text(), 'link': tag["href"]} for tag in soup.select("a.link_cropped_no")]
+    soup = bs4.BeautifulSoup(request.text, "html.parser")
+    results = [{'text': tag.get_text(), 'link': tag["href"]}
+               for tag in soup.select("a.link_cropped_no")]
     return results
 
 
-if __name__ == "__main__":
-    print("Enter your response:", end='')
+def main():
+    print("Enter your response: ", end='')
     request = input()
     results = search(request)
-    print('\n'.join((str(num) + ". " + result['text'] for (num, result) in enumerate(results))))
+    print('\n'.join(
+        (str(num) + ". " + result['text']
+         for (num, result) in enumerate(results))
+    ))
 
     flag = True
     while flag:
-        print("Enter your choice:", end='')
+        print("Enter your choice: ", end='')
         try:
             choice = results[int(input())]['link']
         except:
@@ -32,3 +38,6 @@ if __name__ == "__main__":
         else:
             webbrowser.open(choice)
             flag = False
+
+if __name__ == "__main__":
+    main()
